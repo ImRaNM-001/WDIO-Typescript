7eaebd77e34689c7aa425ca126129e36dae616fb
ImRaNM-001 <59203975+ImRaNM-001@users.noreply.github.com>
2023-12-27 04:17:41 +0530

    initial-commit-27-Dec-2023

---
+++ test/pages/secure-page.ts
@@ -0,0 +1,16 @@
+import Page from './page';
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class SecurePage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get flashAlert () {
+        return $('#flash');
+    }
+}
+
+// module.exports = new SecurePage();
+export default new SecurePage();
